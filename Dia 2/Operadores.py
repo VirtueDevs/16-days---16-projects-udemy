1fca28c21956e15888e742a668784fa671cd2388
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-04 15:11:15 -0500

    Upload Day2

---
+++ Dia 2/Operadores.py
@@ -0,0 +1,18 @@
+x = 6
+y = 2
+z = 7
+
+print(f"{x} mas {y} es igual {x+y}")
+print(f"{x} menos {y} es igual {x-y}")
+print(f"{x} por {y} es igual {x*y}")
+print(f"{x} entre {y} es igual {x/y}")
+
+#Division al piso
+print(f"{z} divido al piso de {y} es igual a {z//y}")
+#módulo
+print(f"{z} modulo de {y} es igual {z%y}")
+#Exponer
+print(f"{x} elevado a la {y} es igual a {x**y}")
+print(f"{x} elevado a la {3} es igual a {x**3}")
+#Radicales
+print(f"La raíz cuadrada de {x} es {x**0.5}")
