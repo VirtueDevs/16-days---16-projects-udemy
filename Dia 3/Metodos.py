bcb57ed0ea79b010c20aa31de8b2a2071a6ef7ef
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-06 21:28:27 -0500

    Day3

---
+++ Dia 3/Metodos.py
@@ -0,0 +1,41 @@
+texto = "Este es el texto de Andrés"
+
+resultado = texto.upper()
+
+print(resultado)
+
+resultado = texto[2].upper()
+
+print(resultado)
+
+resultado = texto.lower()
+
+print(resultado)
+
+resultado = texto.split()
+
+print(resultado)
+
+resultado = texto.split("t")
+
+print(resultado)
+
+a = "Aprender"
+b = "Python"
+c = "es"
+d = "genial"
+e = " ".join([a,b,c,d])
+
+print(e)
+
+resultado = texto.find("s")
+
+print(resultado)
+
+resultado = texto.replace("Andrés","Mary")
+
+print(resultado)
+
+resultado = texto.replace("e","a")
+
+print(resultado)
