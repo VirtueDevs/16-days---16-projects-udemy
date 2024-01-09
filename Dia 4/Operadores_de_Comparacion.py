a76a463d87d1b25c8f0f6fb0367fd551690e5b9f
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-08 23:57:12 -0500

    Day4

---
+++ Dia 4/Operadores_de_Comparacion.py
@@ -0,0 +1,22 @@
+mi_bool = 10 == 25
+print(mi_bool)
+mi_bool = 10 == 10
+print(mi_bool)
+
+mi_bool = 5+5 == 18-8
+print(mi_bool)
+
+mi_string = 'blanco' == 'negro'
+print(mi_string)
+mi_string = 'blanco' == 'Blanco'
+print(mi_string)
+mi_string = 'blanco' == 'Blanco'.lower()
+print(mi_string)
+
+mi_num = '100' == 100
+print(mi_num)
+mi_num = 100.0 == 100
+print(mi_num)
+
+mi_num = 100.0 != 100
+print(mi_num)
