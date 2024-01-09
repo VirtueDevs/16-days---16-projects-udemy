89c3b933a2f671af54258c7a2a6f4c7271a65eeb
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-09 18:18:23 -0500

    Day4

---
+++ Dia 4/Rango.py
@@ -0,0 +1,8 @@
+for numero in range(1,5): #(1 -> desde dónde, 5 -> hasta donde)
+    print(numero)
+
+for numero in range (20,31,2): #(x,y,2 -> saltos )
+    print(numero)
+
+lista = list(range(1,101))
+print(lista)
