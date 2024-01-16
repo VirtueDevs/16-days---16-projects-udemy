4891ce2de49bdda063d206e1b605c20e1d3652b8
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-15 20:10:17 -0500

    Day5

---
+++ Dia 5/Ayuda.py
@@ -0,0 +1,5 @@
+dic = {'Clave1':100,'Clave2':500}
+
+a = dic.popitem()
+print(a)
+print(dic)
---
+++ Dia 5/Funciones.py
@@ -0,0 +1,13 @@
+def saludar_persona():
+    ''' Esta funcion saluda a las personas
+        otra linea'''
+    print('Hola')
+
+def saludo_personalizado(nombre):
+    ''' Esta funcion saluda a las personas
+        otra linea'''
+    print(f'Hola {nombre}')
+
+saludar_persona()
+saludo_personalizado('Juan')
+saludo_personalizado('Andres')
