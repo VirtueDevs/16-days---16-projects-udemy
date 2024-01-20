a9f4f895132e3075e84730206db4be10e2a12eca
Andres Avila <andrese.avilap@utadeo.edu.co>
2024-01-19 21:19:08 -0500

    Day5

--- Dia 5/Ejercicio_Practico_4.py
+++ Dia 5/Ejercicio_Practico_4.py
@@ -1,8 +1,15 @@
 def contar_primos(numero):
+    primos = []
     for elemento in range(2, numero):
-        if numero % elemento == 0:
-            return False
-    return True
-
-print(contar_primos(12))
-
+        es_primo = True
+        for i in range(2, int(elemento ** 0.5) + 1):
+            if elemento % i == 0:
+                es_primo = False
+                break
+        if es_primo:
+            primos.append(elemento)
+    
+    return len(primos)
+            
+print(contar_primos(10))
+            
